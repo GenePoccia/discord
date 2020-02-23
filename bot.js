db88c33d2ad78c9716252620e823709b4b8c6a45
unknown <pocciaeugenio@gmail.com>
2020-02-23 17:46:06 -0500

    added matt filter

--- bot.js
+++ bot.js
@@ -20,7 +20,15 @@ bot.on('ready', function (evt) {
 
 //messages
 bot.on('message', function (user, userID, channelID, message, evt) {
-    if (message.substring(0, 1) == TEXT_COMMAND) {
+    //Handles messages from Matt
+    if (userID === '334508571037401090') {
+        bot.sendMessage({
+            to: channelID,
+            message: 'Excuse me Matt31, can you stop trolling?'
+        })
+    }
+    //handles regular messages
+    else if (message.substring(0, 1) == TEXT_COMMAND) {
         textHandler.send(message, bot, channelID)
      }
      else if (message.substring(0, 1) == MUSIC_COMMAND) {
